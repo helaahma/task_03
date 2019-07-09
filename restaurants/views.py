bdd31e849a87df871995c72f264ab8e2118afdb7
Ahmad Helal <you@example.com>
2019-07-09 20:16:35 +0300

    Done

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,6 +6,21 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
+    "my_list": [
+
+    {
+    "restaurant_name":"five guys",
+    "food_type" : "fast food"
+    },
+    {
+    "restaurant_name":"Meat co.",
+    "food_type" : "stake house"
+    },
+    {
+    "restaurant_name" : "Dai Forni",
+    "food_type" : "Italian food"
+    }
+    ]
 
     }
     return render(request, 'list.html', context)
@@ -14,6 +29,10 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
-
+    "my_object":
+    {
+    "restaurant_name":"five guys",
+    "food_type":"fast food"
+    },
     }
     return render(request, 'detail.html', context)
